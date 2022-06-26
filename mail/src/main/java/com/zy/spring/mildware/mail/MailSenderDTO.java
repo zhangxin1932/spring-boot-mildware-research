033b168f702b48abd0ed668bf99cb99b22d12245
zhangxin1932 <psy_zhangxin@outlook.com>
2022-06-26 19:42:30 +0800

    init mail

--- mail/src/main/java/com/zy/spring/mildware/mail/MailSenderDTO.java
+++ mail/src/main/java/com/zy/spring/mildware/mail/MailSenderDTO.java
@@ -2,7 +2,6 @@ package com.zy.spring.mildware.mail;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
@@ -11,21 +10,17 @@ public class MailSenderDTO implements Serializable {
     /**
      * 发件人账号
      */
-    @NotEmpty(message = "username 不能为空")
     private String username;
     /**
      * 发件人密码
      */
-    @NotEmpty(message = "password 不能为空")
     private String password;
     /**
      * Host
      */
-    @NotEmpty(message = "host 不能为空")
     private String host;
     /**
      * 协议类型
      */
-    @NotEmpty(message = "protocol 不能为空")
     private String protocol;
 }
